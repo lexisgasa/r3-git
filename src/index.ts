161017ca23976c46fed6c770f5689f3be7e2a88e
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-06-25 23:23:34 +0200

    Fix folder name to 'src'

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+type Structure = {
+  id: string;
+  title: string;
+  description: string;
+  isVisible: boolean;
+};
+
+const test: Structure = {
+  id: "01",
+  title: "Jugando con Git",
+  description:
+    "Creando un objeto para trastear con los comandos de Git y GitHub",
+  isVisible: false,
+};
